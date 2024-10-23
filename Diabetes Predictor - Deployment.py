2f3973fc114526233fb761b628c75391c1bb792a
James_Muguro <kamandemuguro@gmail.com>
2024-10-23 09:45:50 +0300

    Improvements to enhance the robustness and maintainability of the code

--- Diabetes Predictor - Deployment.py
+++ Diabetes Predictor - Deployment.py
@@ -2,35 +2,51 @@
 import numpy as np
 import pandas as pd
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, accuracy_score
 
 # Loading the dataset
 df = pd.read_csv('kaggle_diabetes.csv')
 
 # Renaming DiabetesPedigreeFunction as DPF
-df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
+df.rename(columns={'DiabetesPedigreeFunction': 'DPF'}, inplace=True)
 
-# Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
-df_copy = df.copy(deep=True)
-df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+# Replacing the 0 values from ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI'] with NaN
+df[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']].replace(0, np.NaN)
 
-# Replacing NaN value by mean, median depending upon distribution
-df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
-df_copy['BloodPressure'].fillna(df_copy['BloodPressure'].mean(), inplace=True)
-df_copy['SkinThickness'].fillna(df_copy['SkinThickness'].median(), inplace=True)
-df_copy['Insulin'].fillna(df_copy['Insulin'].median(), inplace=True)
-df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
-
-# Model Building
-from sklearn.model_selection import train_test_split
+# Splitting the dataset into features and target variable
 X = df.drop(columns='Outcome')
 y = df['Outcome']
+
+# Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
+# Imputing missing values after splitting
+X_train['Glucose'].fillna(X_train['Glucose'].mean(), inplace=True)
+X_train['BloodPressure'].fillna(X_train['BloodPressure'].mean(), inplace=True)
+X_train['SkinThickness'].fillna(X_train['SkinThickness'].median(), inplace=True)
+X_train['Insulin'].fillna(X_train['Insulin'].median(), inplace=True)
+X_train['BMI'].fillna(X_train['BMI'].median(), inplace=True)
+
+X_test['Glucose'].fillna(X_train['Glucose'].mean(), inplace=True)  # Use the training set mean
+X_test['BloodPressure'].fillna(X_train['BloodPressure'].mean(), inplace=True)
+X_test['SkinThickness'].fillna(X_train['SkinThickness'].median(), inplace=True)
+X_test['Insulin'].fillna(X_train['Insulin'].median(), inplace=True)
+X_test['BMI'].fillna(X_train['BMI'].median(), inplace=True)
+
 # Creating Random Forest Model
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=20)
+classifier = RandomForestClassifier(n_estimators=100, random_state=0)  # Increased n_estimators for potentially better performance
 classifier.fit(X_train, y_train)
+
+# Predicting on the test set
+y_pred = classifier.predict(X_test)
+
+# Evaluating the model
+print("Model Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 
 # Creating a pickle file for the classifier
 filename = 'diabetes-prediction-rfc-model.pkl'
-pickle.dump(classifier, open(filename, 'wb'))
+with open(filename, 'wb') as model_file:
+    pickle.dump(classifier, model_file)
